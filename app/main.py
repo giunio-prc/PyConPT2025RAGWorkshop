636bdb2796044f86b78d3a04dfe46db09bea5d55
Giunio <giuniodl@live.it>
2025-07-26 12:10:06 +0100

    implement api

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request, UploadFile, HTTPException
-from app.controller.controller import add_text_into_db
+from app.controller.controller import add_text_into_db, query_the_agent
 from app.databases.fakedatabase import FakeDatabase
 from app.agents.fakeagent import FakeAgent
 from app.interfaces.database import DatabaseI
+from app.interfaces.agent import AIAgentI
 
 
 @asynccontextmanager
@@ -23,3 +24,11 @@ def add_document(request: Request, file: UploadFile):
         raise HTTPException(status_code=403, detail="format not allowed")
     db: DatabaseI = request.state.db
     add_text_into_db(db=db, text=file.file.read().decode())
+
+
+@app.post("/query-agent")
+def query_agent(request: Request, question: str):
+    agent: AIAgentI = request.state.agent
+    db: DatabaseI = request.state.db
+    answer = query_the_agent(db=db, agent=agent, question=question)
+    return answer
